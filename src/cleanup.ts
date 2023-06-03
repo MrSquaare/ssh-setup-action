6f248516cf9549bee939ec3cfe304784e318c2aa
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2023-06-03 11:42:09 +0200

    fix: remove core import, use es import, format code

--- src/cleanup.ts
+++ src/cleanup.ts
@@ -1,11 +1,10 @@
-const core = require('@actions/core');
-const { execFileSync } = require('child_process');
+import { execFileSync } from "child_process";
 
 try {
-    // Kill the started SSH agent
-    console.log('Stopping SSH agent');
-    execFileSync("ssh-agent", ['-k'], { stdio: 'inherit' });
+  // Kill the started SSH agent
+  console.log("Stopping SSH agent");
+  execFileSync("ssh-agent", ["-k"], { stdio: "inherit" });
 } catch (error: any) {
-    console.log(error.message);
-    console.log('Error stopping the SSH agent, proceeding anyway');
+  console.log(error.message);
+  console.log("Error stopping the SSH agent, proceeding anyway");
 }
